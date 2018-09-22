6030e01b0ad33fc01f5a2ebea7679be429e806d1
werzizu <werzizu@gmail.com>
2018-09-22 23:10:32 +0300

    complete redux store in angular

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,47 @@
-import { renderTodos } from './utils';
+import * as fromStore from "./store";
+import { renderTodos } from "./utils";
 
-const input = document.querySelector('input') as HTMLInputElement;
-const button = document.querySelector('button') as HTMLButtonElement;
-const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
-const todoList = document.querySelector('.todos') as HTMLLIElement;
+const input = document.querySelector("input") as HTMLInputElement;
+const button = document.querySelector("button") as HTMLButtonElement;
+const destroy = document.querySelector(".unsubscribe") as HTMLButtonElement;
+const todoList = document.querySelector(".todos") as HTMLLIElement;
+
+const reducers = {
+  todos: fromStore.reducer
+};
+
+const store = new fromStore.Store(reducers);
+
+console.log(store.value);
 
 button.addEventListener(
-  'click',
+  "click",
   () => {
     if (!input.value.trim()) return;
+    const todo = { label: input.value, complete: false };
 
-    const payload = { label: input.value, complete: false };
-
-    console.log(payload);
-
-    input.value = '';
+    store.dispatch(new fromStore.AddTodo(todo));
+    console.log(todo);
+    input.value = "";
   },
   false
 );
 
-todoList.addEventListener('click', function(event) {
+const unsubscribe = store.subscribe(state => {
+  console.log("render TODOS");
+  renderTodos(state.todos.data);
+});
+
+destroy.addEventListener("click", unsubscribe, false);
+
+todoList.addEventListener("click", function(event) {
   const target = event.target as HTMLButtonElement;
-  if (target.nodeName.toLowerCase() === 'button') {
+  if (target.nodeName.toLowerCase() === "button") {
     console.log(target);
+    const todo = JSON.parse(target.getAttribute("data-todo") as any);
+    store.dispatch(new fromStore.RemoveTodo(todo));
   }
 });
+
+const unsub = store.subscribe(state => console.log("STATE:::", state));
+unsub();
